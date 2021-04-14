364db6fab5a474847e7034ce0e10814f690a735a
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-14 00:54:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,11 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN")
 
+# We used Nokogiri to get the HTML of a web page. We used the element inspector in the browser
+# to ID the CSS selector of the data we wanted to scrape. We used the .css Nokogiri method, along with that CSS selector, 
+# to grab the element that contains our desired data. Finally, we used the .text method to retrieve the desired text.
+
+
+
 
 html =  open("https://flatironschool.com/")
